4764b4100d2bb7e16a5525e5dfbdea4fdb627646
77anandkumar <77anandkumar@gmail.com>
2019-07-24 17:05:56 +0900

    commit 5

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
 	c := hello3(hello2(hello1()))
 	fmt.Println(c("its c"))
+	fmt.Println("commit5")
 
 }
 
@@ -20,30 +22,25 @@ type Server struct {
 }
 
 func hello1() func(string) int {
-	return (
-		func(string2 string) int {
-			fmt.Println(string2)
-			return 10
-		})
+	return (func(string2 string) int {
+		fmt.Println(string2)
+		return 10
+	})
 }
 
-
-
-func hello2(next func(string)int ) func(string) int {
-	a:= next("calling from hello2")
-	return (
-		func(string2 string) int {
-			fmt.Println(string2)
-			return 10+a
-		})
+func hello2(next func(string) int) func(string) int {
+	a := next("calling from hello2")
+	return (func(string2 string) int {
+		fmt.Println(string2)
+		return 10 + a
+	})
 }
 
-func hello3(next func(string) int) func(string) int{
+func hello3(next func(string) int) func(string) int {
 	a := next("calling from hello3")
-	return(
-		func(s string) int {
-			fmt.Println(s)
-			return 10+a
+	return (func(s string) int {
+		fmt.Println(s)
+		return 10 + a
 
-		})
+	})
 }
